83713105a1a4fab266d1c3fcf543f18a17bb6233
yogimardilah <yogimardilah19@gmail.com>
2021-10-26 16:46:25 +0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2><Strong> Delicious Food Menu,</Strong> <br />In Your Gadget</h2>
+          <p>Ayo segera pilih dan pesan makan faforit anda</p>
+          <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>Pesan</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/hero.png" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Foods.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+    <h2>daftar makanan</h2>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Foods",
+  components: {
+    Navbar,
+  },
+};
+</script>
