36cdd784dd5595c1841719ba5fc144f41423ee7f
yogimardilah <yogimardilah19@gmail.com>
2021-10-28 09:43:59 +0700

    penyesuaian design web dan mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="row mt-4">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2><Strong> Delicious Food Menu,</Strong> <br />In Your Gadget</h2>
-          <p>Ayo segera pilih dan pesan makan faforit anda</p>
-          <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right>Pesan</button>
+  <div class="hero">
+    <div class="d-none d-md-block">
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h2><Strong> Delicious Food Menu,</Strong> <br />In Your Gadget</h2>
+            <p>Ayo segera pilih dan pesan makan faforit anda</p>
+            <button class="btn btn-lg btn-success">
+              <b-icon-arrow-right></b-icon-arrow-right>Pesan
+            </button>
+          </div>
         </div>
       </div>
+      <div class="col-md-6">
+        <img src="../assets/images/hero.png" width="100%" />
+      </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/hero.png" width="100%" />
+    </div>
+
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <Strong> Delicious Food Menu,</Strong> <br />In Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makan faforit anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right>Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </div>
   </div>
 </template>
