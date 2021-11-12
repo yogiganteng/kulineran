8ba910f817a667aebd982eefe79324538fd0815b
yogimardilah <yogimardilah19@gmail.com>
2021-11-12 17:34:13 +0700

    create card product from API json server list product

--- src/views/Foods.vue
+++ src/views/Foods.vue
@@ -1,17 +1,61 @@
 <template>
-  <div>
+  <div class="home">
     <Navbar />
-    <h2>daftar makanan</h2>
+    <div class="container">
+      <Hero />
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            Best
+            <strong>Foods</strong>
+          </h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye></b-icon-eye>Lihat Semua
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
-  name: "Foods",
+  name: "Home",
   components: {
     Navbar,
+    Hero,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error))
   },
 };
 </script>
